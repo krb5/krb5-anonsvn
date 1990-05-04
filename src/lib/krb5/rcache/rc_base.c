b355098c0cf9505e58ff7c624f367913803c4772
jtkohl <jtkohl@dc483132-0cff-0310-8789-dd5450dbe970>
1990-05-04 14:42:42 +0000

    add FREE() fix up type names fix up typehead stuff for better linking add krb5_rc_default, krb5_rc_resolve_full ./
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/unlabeled-5.0.1@722 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/rcache/rc_base.c
+++ src/lib/krb5/rcache/rc_base.c
@@ -11,19 +11,22 @@ Please address any questions or comments to the author at brnstnd@acf10.nyu.edu.
 #endif
 #include "rc_base.h"
 
-static struct krb5_rc_typelist
+#define FREE(x) ((void) free((char *) (x)))
+
+struct krb5_rc_typelist
  {
-  struct krb5_rc_type *ops;
+  krb5_rc_ops *ops;
   struct krb5_rc_typelist *next;
- }
-*typehead = (struct krb5_rc_typelist *) 0;
+ };
+static struct krb5_rc_typelist krb5_rc_typelist_dfl = { &krb5_rc_dfl_ops, 0 };
+static struct krb5_rc_typelist *typehead = &krb5_rc_typelist_dfl;
 
 #ifdef SEMAPHORE
 semaphore ex_typelist = 1;
 #endif
 
 krb5_error_code krb5_rc_register_type(ops)
-struct krb5_rc_type *ops;
+krb5_rc_ops *ops;
 {
  struct krb5_rc_typelist *t;
 #ifdef SEMAPHORE
@@ -51,7 +54,7 @@ struct krb5_rc_type *ops;
 }
 
 krb5_error_code krb5_rc_resolve_type(id, type)
-krb5_RC *id;
+krb5_rcache *id;
 char *type;
 {
  struct krb5_rc_typelist *t;
@@ -71,7 +74,7 @@ char *type;
 }
 
 char *krb5_rc_get_type(id)
-krb5_RC id;
+krb5_rcache id;
 {
  return id->ops->type;
 }
@@ -89,7 +92,6 @@ char *krb5_rc_default_type()
    return "dfl";
 }
 
-#ifdef notdef
 #ifdef __STDC__
 char *krb5_rc_default_name(void)
 #else
@@ -102,4 +104,55 @@ char *krb5_rc_default_name()
  else
    return (char *) 0;
 }
-#endif
+
+krb5_error_code
+krb5_rc_default(id)
+krb5_rcache *id;
+{
+    krb5_error_code retval;
+
+    if (!(*id = (krb5_rcache )malloc(sizeof(**id))))
+	return KRB5_RC_MALLOC;
+
+    if (retval = krb5_rc_resolve_type(id, krb5_rc_default_type())) {
+	FREE(*id);
+	return retval;
+    }
+    if (retval = krb5_rc_resolve(*id, krb5_rc_default_name()))
+	FREE(*id);
+    return retval;
+}
+
+
+krb5_error_code krb5_rc_resolve_full(id, string_name)
+krb5_rcache *id;
+char *string_name;
+{
+    char *type;
+    char *residual;
+    krb5_error_code retval;
+
+    if (!(residual = strchr(string_name,':')))
+	return KRB5_RC_PARSE;
+ 
+    if (!(type = malloc(residual - string_name + 1)))
+	return KRB5_RC_MALLOC;
+    (void) strncpy(type,string_name,residual - string_name);
+    type[residual - string_name] = '\0';
+
+    if (!(*id = (krb5_rcache) malloc(sizeof(**id)))) {
+	FREE(type);
+	return KRB5_RC_MALLOC;
+    }
+
+    if (retval = krb5_rc_resolve_type(id,type)) {
+	FREE(type);
+	FREE(*id);
+	return retval;
+    }
+    FREE(type);
+    if (retval = krb5_rc_resolve(*id,residual + 1))
+	FREE(*id);
+    return retval;
+}
+
