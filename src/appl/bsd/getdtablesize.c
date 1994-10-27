58420f60b8c1c369a364edfe6102158ed652d068
(no author) <(no author)@dc483132-0cff-0310-8789-dd5450dbe970>
1994-10-27 12:39:23 +0000

    This commit was manufactured by cvs2svn to create tag 'SECURE_11_EA_2'.
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/SECURE_11_EA_2@4589 dc483132-0cff-0310-8789-dd5450dbe970

--- src/appl/bsd/getdtablesize.c
+++
@@ -1,8 +0,0 @@
-/* Placed in the Public Domain by Mark Eichin, Cygnus Support 1994 */
-
-#include <sys/resource.h>
-int getdtablesize() {
-  struct rlimit rl;
-  getrlimit(RLIMIT_NOFILE, &rl);
-  return rl.rlim_cur;
-}
