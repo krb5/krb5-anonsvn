73431caac9ac25786fb4599b17cfcec3bc88973f
tlyu <tlyu@dc483132-0cff-0310-8789-dd5450dbe970>
1996-01-16 22:07:41 +0000

    stock autoconf-2.10
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/FSF@7328 dc483132-0cff-0310-8789-dd5450dbe970

--- src/util/autoconf/autoconf.sh
+++ src/util/autoconf/autoconf.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#! /bin/sh
 # autoconf -- create `configure' using m4 macros
-# Copyright (C) 1992, 1993, 1994 Free Software Foundation, Inc.
+# Copyright (C) 1992, 1993, 1994, 1996 Free Software Foundation, Inc.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,81 +20,105 @@
 # With one arg, create a configure script on standard output from
 # the given template file.
 
-usage="Usage: autoconf [-h] [--help] [-m dir] [--macrodir=dir] 
-                [-v] [--version] [template-file]" 
+usage="\
+Usage: autoconf [-h] [--help] [-m dir] [--macrodir=dir]
+       [-l dir] [--localdir=dir] [--version] [template-file]"
 
 # NLS nuisances.
+# Only set `LANG' and `LC_ALL' to "C" if already set.
 # These must not be set unconditionally because not all systems understand
 # e.g. LANG=C (notably SCO).
-if test "${LC_ALL+set}" = 'set' ; then LC_ALL=C; export LC_ALL; fi
-if test "${LANG+set}"   = 'set' ; then LANG=C;   export LANG;   fi
+if test "${LC_ALL+set}" = set; then LC_ALL=C; export LC_ALL; fi
+if test "${LANG+set}"   = set; then LANG=C;   export LANG;   fi
 
-test -z "${AC_MACRODIR}" && AC_MACRODIR=@datadir@
-test -z "${M4}" && M4=@M4@
+: ${AC_MACRODIR=@datadir@}
+: ${M4=@M4@}
+: ${AWK=@AWK@}
 case "${M4}" in
 /*) # Handle the case that m4 has moved since we were configured.
     # It may have been found originally in a build directory.
     test -f "${M4}" || M4=m4 ;;
 esac
 
-tmpout=/tmp/acout.$$
+: ${TMPDIR=/tmp}
+tmpout=${TMPDIR}/acout.$$
+localdir=
+show_version=no
 
-print_version=
 while test $# -gt 0 ; do
-   case "z${1}" in 
-      z-h | z--help | z--h* )
+   case "${1}" in
+      -h | --help | --h* )
          echo "${usage}" 1>&2; exit 0 ;;
-      z--macrodir=* | z--m*=* )
+      --localdir=* | --l*=* )
+         localdir="`echo \"${1}\" | sed -e 's/^[^=]*=//'`"
+         shift ;;
+      -l | --localdir | --l*)
+         shift
+         test $# -eq 0 && { echo "${usage}" 1>&2; exit 1; }
+         localdir="${1}"
+         shift ;;
+      --macrodir=* | --m*=* )
          AC_MACRODIR="`echo \"${1}\" | sed -e 's/^[^=]*=//'`"
          shift ;;
-      z-m | z--macrodir | z--m* ) 
+      -m | --macrodir | --m* )
          shift
          test $# -eq 0 && { echo "${usage}" 1>&2; exit 1; }
          AC_MACRODIR="${1}"
          shift ;;
-      z-v | z--version | z--v* )
-         print_version="-DAC_PRINT_VERSION"
-         infile=/dev/null tmpout=/dev/null
-         shift ;;
-      z-- )     # Stop option processing
+      --version | --v* )
+         show_version=yes; shift ;;
+      -- )     # Stop option processing
         shift; break ;;
-      z- )	# Use stdin as input.
+      - )	# Use stdin as input.
         break ;;
-      z-* )
+      -* )
         echo "${usage}" 1>&2; exit 1 ;;
       * )
         break ;;
    esac
 done
 
-if test -z "$print_version"; then
-  case $# in
-    0) infile=configure.in ;;
-    1) infile="$1" ;;
-    *) echo "$usage" >&2; exit 1 ;;
-  esac
-
-  trap 'rm -f $tmpin $tmpout; exit 1' 1 2 15
-
-  if test z$infile = z-; then
-    tmpin=/tmp/acin.$$
-    infile=$tmpin
-    cat > $infile
-  elif test ! -s "${infile}"; then
-    echo "autoconf: ${infile}: No such file or directory" >&2
-    exit 1
-  fi
+if test $show_version = yes; then
+  version=`sed -n 's/define.AC_ACVERSION.[ 	]*\([0-9.]*\).*/\1/p' \
+    $AC_MACRODIR/acgeneral.m4`
+  echo "Autoconf version $version"
+  exit 0
 fi
 
-MACROFILES="${AC_MACRODIR}/acgeneral.m4 ${AC_MACRODIR}/acspecific.m4"
-test -r ${AC_MACRODIR}/aclocal.m4 \
-   && MACROFILES="${MACROFILES} ${AC_MACRODIR}/aclocal.m4"
-test -r aclocal.m4 && MACROFILES="${MACROFILES} aclocal.m4"
-MACROFILES="${print_version} ${MACROFILES}"
+case $# in
+  0) infile=configure.in ;;
+  1) infile="$1" ;;
+  *) echo "$usage" >&2; exit 1 ;;
+esac
 
-$M4 $MACROFILES $infile > $tmpout || { st=$?; rm -f $tmpin $tmpout; exit $st; }
+trap 'rm -f $tmpin $tmpout; exit 1' 1 2 15
 
-test -n "$print_version" && exit 0
+tmpin=${TMPDIR}/acin.$$ # Always set this, to avoid bogus errors from some rm's.
+if test z$infile = z-; then
+  infile=$tmpin
+  cat > $infile
+elif test ! -r "$infile"; then
+  echo "autoconf: ${infile}: No such file or directory" >&2
+  exit 1
+fi
+
+if test -n "$localdir"; then
+  use_localdir="-I$localdir -DAC_LOCALDIR=$localdir"
+else
+  use_localdir=
+fi
+
+# Use the frozen version of Autoconf if available.
+r= f=
+# Some non-GNU m4's don't reject the --help option, so give them /dev/null.
+case `$M4 --help < /dev/null 2>&1` in
+*reload-state*) test -r $AC_MACRODIR/autoconf.m4f && { r=--reload f=f; } ;;
+*traditional*) ;;
+*) echo Autoconf requires GNU m4 1.1 or later >&2; rm -f $tmpin; exit 1 ;;
+esac
+
+$M4 -I$AC_MACRODIR $use_localdir $r autoconf.m4$f $infile > $tmpout ||
+  { rm -f $tmpin $tmpout; exit 2; }
 
 # You could add your own prefixes to pattern if you wanted to check for
 # them too, e.g. pattern="AC_\|ILT_", except that UNIX sed doesn't do
@@ -102,19 +126,30 @@ test -n "$print_version" && exit 0
 pattern="AC_"
 
 status=0
-if grep "${pattern}" $tmpout > /dev/null 2>&1; then
+if grep "^[^#]*${pattern}" $tmpout > /dev/null 2>&1; then
   echo "autoconf: Undefined macros:" >&2
-  grep "${pattern}" $tmpout | sed "s/.*\(${pattern}[_A-Z0-9]*\).*/\1/" |
-    while read name; do
-      grep -n $name $infile /dev/null
+  sed -n "s/^[^#]*\\(${pattern}[_A-Za-z0-9]*\\).*/\\1/p" $tmpout |
+    while read macro; do
+      grep -n "^[^#]*$macro" $infile /dev/null
+      test $? -eq 1 && echo >&2 "***BUG in Autoconf--please report*** $macro"
     done | sort -u >&2
   status=1
 fi
 
-case $# in
-  0) cat $tmpout > configure; chmod +x configure ;;
-  1) cat $tmpout ;;
-esac
+if test $# -eq 0; then
+  exec 4> configure; chmod +x configure
+else
+  exec 4>&1
+fi
+
+# Put the real line numbers into configure to make config.log more helpful.
+$AWK '
+/__oline__/ { printf "%d:", NR + 1 }
+           { print }
+' $tmpout | sed '
+/__oline__/s/^\([0-9][0-9]*\):\(.*\)__oline__/\2\1/
+' >&4
 
 rm -f $tmpout
+
 exit $status
