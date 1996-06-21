9bcc93571b24e352d64832f5a601efb24081d7f5
(no author) <(no author)@dc483132-0cff-0310-8789-dd5450dbe970>
1996-06-21 02:17:37 +0000

    This commit was manufactured by cvs2svn to create tag 'for_audit_960614'.
    git-svn-id: svn://anonsvn.mit.edu/krb5/tags/for_audit_960614@8429 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kadmin/v4server/unit-test/getpid.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# tcl sucks big fat hairy rocks
-
-$PS_ALL | awk "/$1/"' && !/awk/ && !/getpid/ { print $2 }'
--- src/kadmin/v4server/unit-test/remove_changepw_perms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# tcl sucks big fat hairy rocks
-
-ed /krb5/ovsec_adm.acl <<EOF >/dev/null 2>&1
-g/changepw\/kerberos/s/^/#/
-w
-q
-EOF
