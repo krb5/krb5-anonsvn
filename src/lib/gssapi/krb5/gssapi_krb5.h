ca36d2c9ee893846ce3d1f8a616741fac9af28c9
marc <marc@dc483132-0cff-0310-8789-dd5450dbe970>
1996-07-05 19:28:03 +0000

    * gssapi_krb5.h: Add declarations for _old mech set, and _both  	mech set
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/OV_9510_INTEGRATION@8554 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/krb5/gssapi_krb5.h
+++ src/lib/gssapi/krb5/gssapi_krb5.h
@@ -33,6 +33,8 @@
 extern const gss_OID_desc * const gss_mech_krb5;
 extern const gss_OID_desc * const gss_mech_krb5_old;
 extern const gss_OID_set_desc * const gss_mech_set_krb5;
+extern const gss_OID_set_desc * const gss_mech_set_krb5_old;
+extern const gss_OID_set_desc * const gss_mech_set_krb5_both;
 
 extern const gss_OID_desc * const gss_nt_krb5_name;
 extern const gss_OID_desc * const gss_nt_krb5_principal;
