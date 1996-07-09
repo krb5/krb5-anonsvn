6d53a97b610166f18851f3abfcd3acf2ebeb822c
bjaspan <bjaspan@dc483132-0cff-0310-8789-dd5450dbe970>
1996-07-09 17:25:54 +0000

    * do-test.pl: rewrite to use kdb5_util instead of kdbkeys
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/OV_9510_INTEGRATION@8570 dc483132-0cff-0310-8789-dd5450dbe970

--- src/kadmin/kdbkeys/do-test.pl
+++ src/kadmin/kdbkeys/do-test.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/afs/athena/contrib/perl/p
 
 #
 # $Id$
@@ -20,15 +20,37 @@ for ($i=0; $i<$ARGV[0]; $i++) {
     system("$INITDB > /dev/null 2>&1") &&
 	die "Error in init_db\n";
 
-    open(KEYS,"./kdbkeys|") || die "Couldn't run ./kdbkeys: $!\n";
+    open(KEYS,"../dbutil/kdb5_util -R dump_db|") ||
+	die "Couldn't run kdb5_util: $!\n";
+    chop($header = <KEYS>);
+    if ($header ne "kdb5_util load_dump version 4") {
+	die "Cannot operate on dump version \"$header\"; version 4 required.";
+    }
     while(<KEYS>) {
-	next if ((!/^ovsec_adm\//) && (!/^krbtgt/));
+	next if ((!/^princ.*kadmin\//) && (!/^princ.*krbtgt/));
 
-	print if $debug;
+	print if $debug > 1;
 
 	split;
 
-	die "Duplicated key $_[0] = $_[1]\n" if $keys{$_[1]}++;
+	$princ = $_[6];
+	$nkeys = $_[4];
+	$ntls = $_[3];
+	print "$princ: nkeys $nkeys, ntls $ntls\n" if $debug;
+	for ($j = 15 + $ntls*3; $nkeys > 0; $nkeys--) {
+	    $ver = $_[$j++];
+	    $kvno = $_[$j++];
+	    $keytype = $_[$j++];
+	    $keylen = $_[$j++];
+	    $keydata = $_[$j++];
+	    $j += 3 if ($ver > 1);
+
+	    print "$princ, ver $ver, kvno $kvno, type $keytype, len $keylen, "
+		. "data $keydata\n" if $debug;
+	    
+	    die "Duplicated key $princ = $keydata\n" if
+		$keys{$keydata}++;
+	}
     }
     close(KEYS);
 }
