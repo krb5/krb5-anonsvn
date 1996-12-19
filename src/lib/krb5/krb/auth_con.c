96526ab93b7a4d1baacc91bbced57b25189a97f2
probe <probe@dc483132-0cff-0310-8789-dd5450dbe970>
1996-12-19 20:56:30 +0000

    Export krb5_auth_con_getkey in the Windows DLL (for gssapi)
    git-svn-id: svn://anonsvn.mit.edu/krb5/branches/V1_0_WIN32_BRANCH@9661 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/krb5/krb/auth_con.c
+++ src/lib/krb5/krb/auth_con.c
@@ -183,7 +183,7 @@ krb5_auth_con_setuseruserkey(context, auth_context, keyblock)
     return(krb5_copy_keyblock(context, keyblock, &(auth_context->keyblock)));
 }
 
-krb5_error_code
+KRB5_DLLIMP krb5_error_code KRB5_CALLCONV
 krb5_auth_con_getkey(context, auth_context, keyblock)
     krb5_context      	  context;
     krb5_auth_context 	  auth_context;
