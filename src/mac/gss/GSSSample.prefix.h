a280e4a10f6bb5868b15f0768fb8f63fd25ce1a3
(no author) <(no author)@dc483132-0cff-0310-8789-dd5450dbe970>
1999-10-26 22:29:19 +0000

    This commit was manufactured by cvs2svn to create tag 'tlyu-tmp'.
    git-svn-id: svn://anonsvn.mit.edu/krb5/tags/tlyu-tmp@11881 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/mac/gss/GSSSample.prefix.h
@@ -0,0 +1,5 @@
+#define GSSAPI_V2
+
+
+//#include <PP_Prefix.h>
+#include <PP_DebugHeaders.h>
---
+++ src/mac/version.r
@@ -0,0 +1,21 @@
+#ifdef mw_rez
+#include <SysTypes.r>
+#include <Types.r>
+#else
+#include "SysTypes.r"
+#include "Types.r"
+#endif
+
+resource 'vers' (1) {
+	0x01, 0x05, beta, 0x01,
+	verUS,
+	"1.0.5b1",
+	"1.0.5b1, Copyright 1996-1998 Massachusetts Institute of Technology"
+};
+
+resource 'vers' (2) {
+	0x01, 0x05, final, 0x01,
+	verUS,
+	"",
+	"Kerberos v5 1.0.5, Copyright 1996-1998 MIT"
+};
