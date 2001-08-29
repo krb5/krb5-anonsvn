27fb753c2b5df76838383fa6e633ac4c9ef443ed
(no author) <(no author)@dc483132-0cff-0310-8789-dd5450dbe970>
2001-08-29 19:27:30 +0000

    This commit was manufactured by cvs2svn to create tag 'Mac_GSSKerberos5_3_2a2'.
    git-svn-id: svn://anonsvn.mit.edu/krb5/tags/Mac_GSSKerberos5_3_2a2@13686 dc483132-0cff-0310-8789-dd5450dbe970

---
+++ src/util/profile/prof_threads.h
@@ -0,0 +1,37 @@
+/*
+ * prof_threads.h
+ * 
+ * Generic interface to (a subset) of the OS threads interface
+ *
+ */
+ 
+#ifndef PROF_THREADS_H
+#define PROF_THREADS_H
+
+#ifdef HAVE_PTHREADS
+
+typedef prof_mutext pthread_mutex_t;
+
+#define prof_mutex_init(mutex)											\
+	pthread_mutex_init (mutex, PTHREAD_PROCESS_PRIVATE)
+#define prof_mutex_lock(mutex)											\
+	pthread_mutex_lock (mutex)
+#define prof_mutex_unlock(mutex)										\
+	pthread_mutex_unlock (mutex)
+#define PROF_MUTEX_INITIALIZER											\
+	PTHREAD_MUTEX_INITIALIZER
+
+/* end HAVE_PTHREADS */
+#elif defined (macintosh)
+
+typedef struct opaque_prof_mutex*	prof_mutex;
+
+int	prof_mutex_init (prof_mutex*		mutex);
+int prof_mutex_lock (prof_mutex*		mutex);
+int prof_mutex_unlock (prof_mutex*		mutex);
+
+#define PROF_MUTEX_INITIALIZER		NULL
+
+#endif /* macintosh */
+
+#endif /* PROF_THREADS_H */
