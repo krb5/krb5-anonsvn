8113841fb543e528a9e7317544e3f9efcb781310
(no author) <(no author)@dc483132-0cff-0310-8789-dd5450dbe970>
2002-08-23 18:03:56 +0000

    This commit was manufactured by cvs2svn to create tag 'tlyu-bthack-tmp'.
    git-svn-id: svn://anonsvn.mit.edu/krb5/tags/tlyu-bthack-tmp@14756 dc483132-0cff-0310-8789-dd5450dbe970

--- src/lib/gssapi/gss_libinit.h
+++
@@ -1,9 +0,0 @@
-#ifndef KRB5_LIBINIT_H
-#define KRB5_LIBINIT_H
-
-#include "gssapi.h"
-
-OM_uint32 gssint_initialize_library (void);
-void gssint_cleanup_library (void);
-
-#endif /* KRB5_LIBINIT_H */
